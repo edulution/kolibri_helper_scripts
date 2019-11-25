f01bd962a2c43a254983873c4e28398d86e93d71
Kapya <kapsakala@gmail.com>
2019-11-25 12:32:13 +0200

    create lessons preparatory steps in function. main loop incomplete

--- create_lessons.py
+++ create_lessons.py
@@ -11,6 +11,9 @@ import datetime
 from django.utils import timezone
 from django.db import connection
 
+# import kolibri content models
+from kolibri.core.content.models import *
+
 # import kolibri auth models
 from kolibri.core.auth.models import *
 
@@ -42,7 +45,6 @@ def get_or_create_classroom(classroomname, facilityname=None):
 			 # if the facility does not exist, raise a value error and terminate the script
 			raise ValueError('There is no Facility called {}. Please the Facility before creating Classes in it'.format(facilityname))
 			sys.exit()
-
 	else:
 	# if the facilityname argument was not passed in, use the default facility as the place to create the class
 		facility_for_class = Facility.get_default_facility()
@@ -65,28 +67,59 @@ def get_or_create_classroom(classroomname, facilityname=None):
 	return class_obj
 
 
-# def create_lessons():
-# 	#get all channels
-# 	channels = ChannelMetadata.objects.all()
+def create_lessons(modulename,classroomname,facilityname=None):
+	
+	# get or create the class to create the lessons for
+	# store a reference to the classroom object if it is created
+	class_for_lessons = get_or_create_classroom(classroomname,facilityname)
 
-# 	# channels are found in topics but have no parent_id and id == parent_id
-# 	# get all topics by getting all contentnodes of type topic which fulfil criteria above
-# 	topics = ContentNode.objects.filter(kind = 'topic').exclude(parent__id_isnull = TRUE)
+	# get a list of the admin and coach accounts on the device
+	admins = FacilityUser.objects.raw("select * from kolibriauth_facilityuser where id in (select user_id from kolibriauth_role where kind in ('admin','coach'))")
+
+	# if there is no admin or coach account on the device
+	# raise an error and terminate the script
+	if len(list(admins))==0:
+		raise ValueError('There is no Admin or Coach account on the device. Cannot create lessons without an Admin or Coach account ')
+		sys.exit()
+	else:
+		# if admin accounts exist, choose the first one and use it to create and assign the lessons
+		admin_for_lessons = admins[0]
 
 
-# 	# get contentnode_ids of all the topics as an array
-# 	topics_ids = [topic.id for topic in topics]  
+	#get all channels with the module passed in
+	channels = ChannelMetadata.objects.raw("select * from content_channelmetadata where id in (select channel_id from channel_module where module = %s)", [modulename.lower()])
 
-# 	# begin looping through topics
-# 	for topic_id in topic_ids:
-# 		nodes_in_topic = ContentNode.objects.filter(parent_id = topic_id)
+	# get a list of all the channel ids for the channels in the above query
+	channel_ids = [channel.id for channel in channels]
 
+	# if there are no channel_ids, then there are no channels that have the module requested
+	# raise an error and terminate the script
+	if len(channel_ids)==0:
+		raise ValueError('There are no channels with a Module called {}. Cannot create lessons without channels '.format(modulename))
+		sys.exit()
 
-		# {
-		#   contentnode_id: string,
-		#   content_id: string,
-		#   channel_id: string
-		# }
+	# need a title, collection and created by to instantiate a lesson. resouces, is_active can be added later
+	# new_lesson = Lesson.objects.create(title = 'orm lesson 2', collection = class_for_lessons, created_by = admin_for_lessons)
+
+	for channel_id in channel_ids:
+		# channels are found in topics but have no parent_id and id == parent_id
+		# get all topics by getting all contentnodes of type topic which fulfil criteria above
+		topics = ContentNode.objects.filter(kind = 'topic', parent_id = channel_id).exclude(parent__id_isnull = TRUE).order_by('sort_order')
+
+		
+		# get contentnode_ids of all the topics as an array
+		topics_ids = [topic.id for topic in topics]  
+
+		# begin looping through topics
+		for topic_id in topic_ids:
+			nodes_in_topic = ContentNode.objects.filter(parent_id = topic_id)
+
+			# structure of content resource in a lesson
+			# {
+			#   contentnode_id: string,
+			#   content_id: string,
+			#   channel_id: string
+			# }
 
 
 
