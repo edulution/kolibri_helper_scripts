dc28d039d7c98d5a3dbe86b402c445b2f9bd6dad
Kapya <kapsakala@gmail.com>
2019-12-19 11:46:05 +0200

    emergency script to enroll all learners into live learners class

---
+++ enroll_all.py
@@ -0,0 +1,21 @@
+# emergency script to enroll all users into the live learners class in case the assign memberships script does not work
+
+import kolibri
+import django
+django.setup()
+
+# import all the helper functions
+from helpers import *
+
+# get a reference to the live learners class
+live_learners_class = Collection.objects.get(name='Live Learners')
+
+# get a list of all the users on the device
+all_users = FacilityUser.objects.all()
+
+# delete all memberships
+Membership.objects.all().delete()	
+
+# assign every user to the live learners class
+for learner in all_users:
+	Membership.objects.create(user = learner, collection = live_learners_class)
