b14d89603f09f978aaa56242433d61c9be6a1cea
Kapya <kapsakala@gmail.com>
2019-12-24 10:12:09 +0200

    sql script to insert missing prerequisites on numeracy playlists

---
+++ insert_prerequisites.sql
@@ -0,0 +1,97 @@
+--Insert prerequisites SQL
+-- To be run on first setup only, or if playlists were re-imported
+
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('1898b5dc-fab5-41d6-bd0b-103cc771fb91',
+       '9367f900-ea32-4f02-93f7-71f52bc8d2f3');
+
+ ​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('ead17523-dcdf-458b-aa9f-b96e5b23bf25',
+       '9eb5720e-f212-4dc6-b251-43719689c201');
+
+ ​​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('4a95fbca-e785-4057-acbd-94c588e7aca6',
+       '543006c7-63ec-4b84-8359-1915666c1c66');
+
+ ​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('ae1cf710-e991-4116-8e3b-e520b5082acd',
+       '9f3b168b-bf77-495e-9830-7061b9ddb527');
+
+ ​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('94073f5d-7b78-41c9-bf1c-04667acb2745',
+       '92eb5036-7a33-42cc-a174-d77cc327075b');
+
+ ​​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('55eddef9-5222-46d9-b541-d78cb71728c4',
+       '6220cd40-8eac-4416-9f00-23a755826ad5');
+
+ ​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('8278f4e5-c3c1-4bd3-9db2-cb7d404317b0',
+       'e559ff4c-86c6-4dae-893c-982665953526');
+
+ ​​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('fbcba573-686f-42f0-9757-83ec248000a7',
+       'd10169a1-aefd-48ec-b76a-2b648f283db3');
+
+
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('3af05d47-e65c-40af-819b-7e98ca2a2e2f',
+       '624c4a94-d2bb-4902-a5e0-9ea3dcfef2b6');
+
+ ​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('a3955e80-29ac-47a2-896c-cee887b979b3',
+       'bcf2517e-1103-43d3-a3d1-9ae9d3dd5f6a');
+
+ ​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('e50f924b-d6e0-4a31-ba16-776ef180ed94',
+       '25c3e9d7-1b2d-41d0-a0a1-908556c640d3');
+
+ ​​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('75dcfc82-9879-405f-8807-7f747c596213',
+       'abfd502a-c074-4b1a-8abf-5ba68a2a4812');
+
+ ​​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('16e64098-81a1-4da1-aa43-71574b1f179e',
+       '01985a2d-1ed0-4298-95ff-7b9985a00993');
+
+ ​​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('25728bcf-fb4b-4dbe-a14f-e44044d76309',
+       '360cd271-ac48-48cc-8928-2bbc120d9502');
+
+ ​​
+DELETE
+FROM content_contentnode_has_prerequisite
+WHERE from_contentnode_id = '7fdb5c82-b1a6-4fe8-8b39-be17a6bb3b41'
+  AND to_contentnode_id = '9d940efc-1149-40f7-aba1-40ae93cc7f00'​​​;
+
+
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('7fdb5c82-b1a6-4fe8-8b39-be17a6bb3b41',
+       '5f356936-1ea2-4379-b109-1ca459ae950c');
+
+ ​​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('c756b5c3-27e6-41b9-acdc-a00e1dfb36a4',
+       'd908e148-f678-49a4-890c-a8c00d5622b3');
+
+ ​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('3af05d47-e65c-40af-819b-7e98ca2a2e2f',
+       '25c3e9d7-1b2d-41d0-a0a1-908556c640d3');
+
+ ​
+INSERT INTO content_contentnode_has_prerequisite(from_contentnode_id, to_contentnode_id)
+VALUES('10d5f907-9eea-48e3-8d5b-b6fecd945e82',
+       '7e1867b2-aba8-489d-b70f-0b0fe6451e98');
