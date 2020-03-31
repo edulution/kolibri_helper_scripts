a9e7996341f9548813fa4638e179e142175d80bf
Kapya <kapsakala@gmail.com>
2020-03-31 23:20:00 +0200

    fixed linting errors as advised by shellcheck

--- assign_learners.sh
+++ assign_learners.sh
@@ -7,4 +7,4 @@
 python ~/.kolibri_helper_scripts/get_live_learners.py
 
 # call the procedure to assign them into the appropriate groups
-PGPASSWORD=$BASELINE_DATABASE_PASSWORD psql -h $BASELINE_DATABASE_HOST -U $BASELINE_DATABASE_USER -d $BASELINE_DATABASE_NAME -c "call spassignmembership();"
+PGPASSWORD=$BASELINE_DATABASE_PASSWORD psql -h "$BASELINE_DATABASE_HOST" -U "$BASELINE_DATABASE_USER" -d "$BASELINE_DATABASE_NAME" -c "call spassignmembership();"
