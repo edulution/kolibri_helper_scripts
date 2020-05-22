c80bbd593af05710cb613765d6be527e56a32d80
Kapya <kapsakala@gmail.com>
2020-05-22 17:10:54 +0200

    change relative path in scripts

--- channel_setup/add_missing_prerequisites.sh
+++ channel_setup/add_missing_prerequisites.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Run the missing prerequisites into the kolibri database
-PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD psql -h "$KOLIBRI_DATABASE_HOST" -U "$KOLIBRI_DATABASE_USER" -d "$KOLIBRI_DATABASE_NAME" -a -f sql/insert_prerequisites.sql
+PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD psql -h "$KOLIBRI_DATABASE_HOST" -U "$KOLIBRI_DATABASE_USER" -d "$KOLIBRI_DATABASE_NAME" -a -f ../sql/insert_prerequisites.sql
