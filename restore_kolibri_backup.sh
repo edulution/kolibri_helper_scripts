2c262a3c7610f72f672e48eace3059d749e05aaf
Kapya <kapsakala@gmail.com>
2020-05-29 12:41:55 +0200

    function to restore a kolibri backup file

--- restore_kolibri_backup.sh
+++ restore_kolibri_backup.sh
@@ -1,4 +1,9 @@
-PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD pg_restore -U $KOLIBRI_DATABASE_USER -h $KOLIBRI_DATABASE_HOST -p $KOLIBRI_DATABASE_PORT -d $KOLIBRI_DATABASE_NAME <backup file>
+#!/bin/bash
 
+# function to drop and recreate the kolibri database then restore the supplied backup file
+restore_kolibri_backup(){
+	echo "Restoring backup file: $1 into Database: $KOLIBRI_DATABASE_NAME"
+	PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD pg_restore -U "$KOLIBRI_DATABASE_USER" -h "$KOLIBRI_DATABASE_HOST" -p "$KOLIBRI_DATABASE_PORT" -d "$KOLIBRI_DATABASE_NAME" --clean --create --no-owner "$1"
+}
 
-PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD pg_restore -U $KOLIBRI_DATABASE_USER -h $KOLIBRI_DATABASE_HOST -p $KOLIBRI_DATABASE_PORT -C -d $KOLIBRI_DATABASE_NAME <backup file>
+restore_kolibri_backup "$1"
