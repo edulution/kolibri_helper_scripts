64c5f593ecd88a44a3c65bb09d427073ee758dee
Kapya <kapsakala@gmail.com>
2020-06-10 21:43:30 +0200

    db connection arg in function. added comments.

--- insert_responses_into_psql.R
+++ insert_responses_into_psql.R
@@ -6,14 +6,14 @@ library(readr)
 library(DBI)
 library(RPostgreSQL)
 
-# connect to responses database 
+# Get the credentials for the baseline database from env variables
 bl_db_name = Sys.getenv("BASELINE_DATABASE_NAME")
 bl_db_host = Sys.getenv("BASELINE_DATABASE_HOST")
 bl_db_user = Sys.getenv("BASELINE_DATABASE_USER")
 bl_db_passwd = Sys.getenv("BASELINE_DATABASE_PASSWORD")
 bl_db_port = Sys.getenv("BASELINE_DATABASE_PORT")
 
-# connect to test responses database
+# Create a database connection object with the credentials above 
 pg <- dbDriver("PostgreSQL")
 conn <-  dbConnect(
 	pg,
@@ -24,18 +24,28 @@ conn <-  dbConnect(
 	password= bl_db_passwd
 	)
 
-# helper function to insert the contents of the responses csv file into the responses table on baseline
-insert_responses <- function(input_file){
-	responses_df <- read.csv(input_file, stringsAsFactors = FALSE)
+# Function to insert the contents of the responses csv file into the responses table on baseline
+insert_responses <- function(input_file, db_conn){
+	# Read the csv file containing the responses
+	responses_df <- read.csv(
+		input_file,
+		stringsAsFactors = FALSE)
+	
+	# Write the responses to the responses table in the baseline db
 	dbWriteTable(
-		conn,
+		db_conn,
 		"responses",
 		responses_df,
 		append = TRUE,
 		row.names = FALSE
 		)
+
+	# disconnect from the database
+	dbDisconnect(conn)
 }
 
+# Accept command line arguments and store them in the input variable
 input<- commandArgs(TRUE)
 
-insert_responses(input)
+# Call the insert_responses function passing in the command line arg and the db connection object
+insert_responses(input, conn)
