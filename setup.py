f50d2ebf31b64e06726f9f9ed4d7ec7af768745a
Kapya <kapsakala@gmail.com>
2020-07-03 08:04:48 +0200

    fixed import statements for create lessons and quizzes

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import kolibri # noqa F401
 import django
 django.setup()
 
-from create_lessons import create_quizzes # noqa E402
-from create_quizzes import create_lessons # noqa E402
+from create_lessons import create_lessons # noqa E402
+from create_quizzes import create_quizzes # noqa E402
 
 # Create lessons and quizzes for the live learners cleass
 # The class will be created if it does not exist
