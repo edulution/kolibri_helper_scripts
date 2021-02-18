8488c68ee81109197cd18de96ecb754be151b348
L3Vyt <47414785+L3Vyt@users.noreply.github.com>
2021-02-18 00:25:01 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,12 @@ django.setup()
 
 from create_lessons import create_lessons  # noqa E402
 from create_quizzes import create_quizzes  # noqa E402
+from create_class import create_class # noqa E402
 
 # Create lessons and quizzes for the live learners cleass
 # The class will be created if it does not exist
 create_quizzes("numeracy", "Live Learners")
 create_lessons("numeracy", "Live Learners")
+create_class("Leaners on Program")
+create_class("Zarchive")
+
