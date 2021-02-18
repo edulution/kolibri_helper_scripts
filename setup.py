57dddd924460a17900c3b41aed62e03f0cd98a90
L3VyM <lmbewe@edulution.org>
2021-02-18 12:03:32 +0200

    imported function from helpers

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ django.setup()
 
 from create_lessons import create_lessons  # noqa E402
 from create_quizzes import create_quizzes  # noqa E402
-from create_class import create_class # noqa E402
+from helpers import get_or_create_classroom # noqa E402
 
 # Create lessons and quizzes for the live learners cleass
 # The class will be created if it does not exist
 create_quizzes("numeracy", "Live Learners")
 create_lessons("numeracy", "Live Learners")
-create_class("Leaners on Program")
-create_class("Zarchive")
+get_or_create_classroom("Leaners on Program")
+get_or_create_classroom("Zarchive")
 
