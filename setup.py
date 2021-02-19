36b025a8be7075487b2c6301e299fabf49094566
Kapya Sakala <kapsakala@gmail.com>
2021-02-19 14:52:22 +0200

    corrected spelling mistake. added comments

--- setup.py
+++ setup.py
@@ -11,6 +11,10 @@ from helpers import get_or_create_classroom # noqa E402
 # The class will be created if it does not exist
 create_quizzes("numeracy", "Live Learners")
 create_lessons("numeracy", "Live Learners")
-get_or_create_classroom("Leaners on Program")
+
+# Create Learners on Program class if it does not exist
+get_or_create_classroom("Learners on Program")
+
+# Create Zarchive class if it does not exist
 get_or_create_classroom("Zarchive")
 
