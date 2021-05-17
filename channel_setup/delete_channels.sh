e96461920d2eab8a7c44a7ac9dbb6e394a1963dc
Kapya Sakala <kapsakala@gmail.com>
2021-05-17 15:36:54 +0200

    added gr7 literacy zambia channel_id to comments

--- channel_setup/delete_channels.sh
+++ channel_setup/delete_channels.sh
@@ -16,7 +16,8 @@
 # Bravo B - b7214b921fd94a1cb758821919bcd3e0
 # Bravo C - 5aee4435135b4039a3a824d96f72bfcb
 # Bravo D - 98ab8048107545da92e3394409955526
-# Grade 7 (Zambia) - 8d368058656544e2b7fe62eb2a632698
+# Grade 7 Numeracy (Zambia) - 8d368058656544e2b7fe62eb2a632698
+# Grade 7 Literacy (Zambia) - 96578ffc06e44a46b021540cb217f9c9
 # Coach Professional Development (Old) - 2c8cd5f3a4694adbb4be45025d9ca3dc
 # Coach Professional Development - 19ea4c94ee484cb0b5bb617f5511f4c1
 
