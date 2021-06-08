521846bdb9b0e12cafa42cb271cc8cb8dc7e3eb7
Kapya Sakala <kapsakala@gmail.com>
2021-06-08 15:18:56 +0200

    added channel subscriptions for levels

--- sql/channel_subscriptions.sql
+++ sql/channel_subscriptions.sql
@@ -1,78 +1,32 @@
+/*Subscribe all classrooms to all numeracy channels*/
 UPDATE kolibriauth_collection
-SET subscriptions = '"[]"'
-WHERE lower(name) LIKE 'learners on program%';
+SET subscriptions = '"[\"f3f6bf4b9c424b6fbd90bece1418a415\",\"3a126f041a9e4127978bb6544a099d0d\",\"03761fe9a2424e9ebd367fbae14dc134\",\"f12e2f67d5894b34bb942484d880c4a6\",\"f6e8f1f569c54bc58ecb54d826cd3783\",\"5126a907e5284345847c8ca749c8cd9b\",\"c32737bf26474dc8a81de3d093075830\",\"0985900c047a41738e796f19d9b57ed5\",\"c0302f6cd0624778bc8dd7a2b91d09b4\",\"ddf25e8fcde84e8998118daffb2d8655\",\"e2217b872f784190a0a55c46e9ee8bc1\",\"f908fac9fe5644b996abe25dfbd530bc\",\"d4b44e6dd4de450ebbc799085ba656e5\",\"763eafc24e684dbe8f54c9a22dd63599\",\"7de1d8eca4874856b508c1a4f51a839e\",\"904d419f53e24b6fb60d2aca7e729683\",\"5b679315a02c40359505fc84701c482d\",\"a5cd9f9f000e41459a95875d7dfd3e68\",\"6c4089232a72447eabbdf83b7c5612a4\",\"28f4920a7b444e29af4f1e8d00ca332b\"]"'
+WHERE kind = 'classroom';
 
 
-UPDATE kolibriauth_collection
-SET subscriptions = '"[\"cc80537886cb498eb564242f44c87723\",\"8784b9f78d584273aff579b246529215\",\"1700bf9e71094857abf36c04a1963004\",\"20113bf1ba074e08bcc7faaca03ade8a\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\",\"8d368058656544e2b7fe62eb2a632698\"]"'
-WHERE name = 'Alpha B';
-
-
-UPDATE kolibriauth_collection
-SET subscriptions = '"[\"1d8f1428da334779b95685c4581186c4\",\"7035e7921ddf489fad4544c814a199fb\",\"cc80537886cb498eb564242f44c87723\",\"8784b9f78d584273aff579b246529215\",\"1700bf9e71094857abf36c04a1963004\",\"20113bf1ba074e08bcc7faaca03ade8a\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\",\"8d368058656544e2b7fe62eb2a632698\"]"'
-WHERE name = 'Alpha D';
-
+/*Subscriptions for each level*/
+/*Each group corresponds to 1 level and can see all the sections for that level and all preceeding levels */
 
 UPDATE kolibriauth_collection
-SET subscriptions = '"[\"b7214b921fd94a1cb758821919bcd3e0\",\"57995474194c4068bfed1ee16108093f\",\"1d8f1428da334779b95685c4581186c4\",\"7035e7921ddf489fad4544c814a199fb\",\"cc80537886cb498eb564242f44c87723\",\"8784b9f78d584273aff579b246529215\",\"1700bf9e71094857abf36c04a1963004\",\"20113bf1ba074e08bcc7faaca03ade8a\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\"]"'
-WHERE name ='Bravo B' ;
+SET subscriptions = '"[\"f3f6bf4b9c424b6fbd90bece1418a415\",\"3a126f041a9e4127978bb6544a099d0d\",\"03761fe9a2424e9ebd367fbae14dc134\",\"f12e2f67d5894b34bb942484d880c4a6\"]"'
+WHERE name = 'Level 1';
 
 
 UPDATE kolibriauth_collection
-SET subscriptions = '"[\"3d6c9d72a2e047d4b7a0ed20699e1b1f\"]"'
-WHERE name = 'Pre-Alpha A';
+SET subscriptions = '"[\"f6e8f1f569c54bc58ecb54d826cd3783\",\"5126a907e5284345847c8ca749c8cd9b\",\"c32737bf26474dc8a81de3d093075830\",\"0985900c047a41738e796f19d9b57ed5\",\"f3f6bf4b9c424b6fbd90bece1418a415\",\"3a126f041a9e4127978bb6544a099d0d\",\"03761fe9a2424e9ebd367fbae14dc134\",\"f12e2f67d5894b34bb942484d880c4a6\"]"'
+WHERE name = 'Level 2';
 
 
 UPDATE kolibriauth_collection
-SET subscriptions = '"[\"98ab8048107545da92e3394409955526\",\"5aee4435135b4039a3a824d96f72bfcb\",\"b7214b921fd94a1cb758821919bcd3e0\",\"57995474194c4068bfed1ee16108093f\",\"1d8f1428da334779b95685c4581186c4\",\"7035e7921ddf489fad4544c814a199fb\",\"cc80537886cb498eb564242f44c87723\",\"8784b9f78d584273aff579b246529215\",\"1700bf9e71094857abf36c04a1963004\",\"20113bf1ba074e08bcc7faaca03ade8a\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\",\"8d368058656544e2b7fe62eb2a632698\"]"'
-WHERE name = 'Bravo D';
+SET subscriptions = '"[\"f6e8f1f569c54bc58ecb54d826cd3783\",\"5126a907e5284345847c8ca749c8cd9b\",\"c32737bf26474dc8a81de3d093075830\",\"0985900c047a41738e796f19d9b57ed5\",\"f3f6bf4b9c424b6fbd90bece1418a415\",\"3a126f041a9e4127978bb6544a099d0d\",\"03761fe9a2424e9ebd367fbae14dc134\",\"f12e2f67d5894b34bb942484d880c4a6\",\"c0302f6cd0624778bc8dd7a2b91d09b4\",\"ddf25e8fcde84e8998118daffb2d8655\",\"e2217b872f784190a0a55c46e9ee8bc1\",\"f908fac9fe5644b996abe25dfbd530bc\"]"'
+WHERE name = 'Level 3';
 
 
 UPDATE kolibriauth_collection
-SET subscriptions = '"[\"20113bf1ba074e08bcc7faaca03ade8a\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\"]"'
-WHERE name = 'Pre-Alpha C';
+SET subscriptions = '"[\"f6e8f1f569c54bc58ecb54d826cd3783\",\"5126a907e5284345847c8ca749c8cd9b\",\"c32737bf26474dc8a81de3d093075830\",\"0985900c047a41738e796f19d9b57ed5\",\"f3f6bf4b9c424b6fbd90bece1418a415\",\"3a126f041a9e4127978bb6544a099d0d\",\"03761fe9a2424e9ebd367fbae14dc134\",\"f12e2f67d5894b34bb942484d880c4a6\",\"c0302f6cd0624778bc8dd7a2b91d09b4\",\"ddf25e8fcde84e8998118daffb2d8655\",\"e2217b872f784190a0a55c46e9ee8bc1\",\"f908fac9fe5644b996abe25dfbd530bc\",\"d4b44e6dd4de450ebbc799085ba656e5\",\"763eafc24e684dbe8f54c9a22dd63599\",\"7de1d8eca4874856b508c1a4f51a839e\",\"904d419f53e24b6fb60d2aca7e729683\"]"'
+WHERE name = 'Level 4';
 
 
 UPDATE kolibriauth_collection
-SET subscriptions = '"[\"1700bf9e71094857abf36c04a1963004\",\"20113bf1ba074e08bcc7faaca03ade8a\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\"]"'
-WHERE name = 'Pre-Alpha D';
-
-
-UPDATE kolibriauth_collection
-SET subscriptions = '"[\"6380a6a98a4c4b268b3147ad1c7ada13\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\"]"'
-WHERE name = 'Pre-Alpha B';
-
-
-UPDATE kolibriauth_collection
-SET subscriptions = '"[\"8784b9f78d584273aff579b246529215\",\"1700bf9e71094857abf36c04a1963004\",\"20113bf1ba074e08bcc7faaca03ade8a\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"8d368058656544e2b7fe62eb2a632698\"]"'
-WHERE name = 'Alpha A';
-
-
-UPDATE kolibriauth_collection
-SET subscriptions = '"[\"7035e7921ddf489fad4544c814a199fb\",\"cc80537886cb498eb564242f44c87723\",\"8784b9f78d584273aff579b246529215\",\"1700bf9e71094857abf36c04a1963004\",\"20113bf1ba074e08bcc7faaca03ade8a\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\",\"8d368058656544e2b7fe62eb2a632698\"]"'
-WHERE name = 'Alpha C';
-
-
-UPDATE kolibriauth_collection
-SET subscriptions = '"[\"57995474194c4068bfed1ee16108093f\",\"1d8f1428da334779b95685c4581186c4\",\"7035e7921ddf489fad4544c814a199fb\",\"cc80537886cb498eb564242f44c87723\",\"8784b9f78d584273aff579b246529215\",\"1700bf9e71094857abf36c04a1963004\",\"20113bf1ba074e08bcc7faaca03ade8a\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\",\"8d368058656544e2b7fe62eb2a632698\"]"'
-WHERE name = 'Bravo A';
-
-
-UPDATE kolibriauth_collection
-SET subscriptions = '"[\"3d6c9d72a2e047d4b7a0ed20699e1b1f\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"20113bf1ba074e08bcc7faaca03ade8a\",\"1700bf9e71094857abf36c04a1963004\",\"8784b9f78d584273aff579b246529215\",\"cc80537886cb498eb564242f44c87723\",\"7035e7921ddf489fad4544c814a199fb\",\"1d8f1428da334779b95685c4581186c4\",\"57995474194c4068bfed1ee16108093f\",\"b7214b921fd94a1cb758821919bcd3e0\",\"5aee4435135b4039a3a824d96f72bfcb\",\"98ab8048107545da92e3394409955526\",\"8d368058656544e2b7fe62eb2a632698\"]"'
-WHERE lower(name) LIKE 'live learners%';
-
-
-UPDATE kolibriauth_collection
-SET subscriptions = '"[]"'
-WHERE lower(name) LIKE 'zarchive%';
-
-
-UPDATE kolibriauth_collection
-SET subscriptions = '"[\"5aee4435135b4039a3a824d96f72bfcb\",\"b7214b921fd94a1cb758821919bcd3e0\",\"57995474194c4068bfed1ee16108093f\",\"7035e7921ddf489fad4544c814a199fb\",\"1d8f1428da334779b95685c4581186c4\",\"8784b9f78d584273aff579b246529215\",\"cc80537886cb498eb564242f44c87723\",\"1700bf9e71094857abf36c04a1963004\",\"20113bf1ba074e08bcc7faaca03ade8a\",\"6380a6a98a4c4b268b3147ad1c7ada13\",\"3d6c9d72a2e047d4b7a0ed20699e1b1f\",\"8d368058656544e2b7fe62eb2a632698\"]"'
-WHERE name = 'Bravo C';
-
-
-UPDATE kolibriauth_collection
-SET subscriptions = '"[\"8d368058656544e2b7fe62eb2a632698\"]"'
-WHERE name = 'Grade 7 (Zambia)';
+SET subscriptions = '"[\"f6e8f1f569c54bc58ecb54d826cd3783\",\"5126a907e5284345847c8ca749c8cd9b\",\"c32737bf26474dc8a81de3d093075830\",\"0985900c047a41738e796f19d9b57ed5\",\"f3f6bf4b9c424b6fbd90bece1418a415\",\"3a126f041a9e4127978bb6544a099d0d\",\"03761fe9a2424e9ebd367fbae14dc134\",\"f12e2f67d5894b34bb942484d880c4a6\",\"c0302f6cd0624778bc8dd7a2b91d09b4\",\"ddf25e8fcde84e8998118daffb2d8655\",\"e2217b872f784190a0a55c46e9ee8bc1\",\"f908fac9fe5644b996abe25dfbd530bc\",\"d4b44e6dd4de450ebbc799085ba656e5\",\"763eafc24e684dbe8f54c9a22dd63599\",\"7de1d8eca4874856b508c1a4f51a839e\",\"904d419f53e24b6fb60d2aca7e729683\",\"5b679315a02c40359505fc84701c482d\",\"a5cd9f9f000e41459a95875d7dfd3e68\",\"6c4089232a72447eabbdf83b7c5612a4\",\"28f4920a7b444e29af4f1e8d00ca332b\"]"'
+WHERE name = 'Level 5';
