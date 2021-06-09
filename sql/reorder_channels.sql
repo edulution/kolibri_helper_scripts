2816b89456e0866c66dc04572d3a856727850a84
Kapya Sakala <kapsakala@gmail.com>
2021-06-09 13:57:09 +0200

    changed statements to reflect order of new level courses

--- sql/reorder_channels.sql
+++ sql/reorder_channels.sql
@@ -1,61 +1,110 @@
-/*SQL to reorder channels in case script does not work*/
-
-/*Pre Alpha Alpha*/
+/*Level 1 - Section 1*/
 UPDATE content_channelmetadata
 SET "order" = 1
-WHERE id = '3d6c9d72-a2e0-47d4-b7a0-ed20699e1b1f';
+WHERE id = 'f3f6bf4b-9c42-4b6f-bd90-bece1418a415';
 
-/*Pre Alpha B*/
+ /*Level 1 - Section 2*/
 UPDATE content_channelmetadata
 SET "order" = 2
-WHERE id = '6380a6a9-8a4c-4b26-8b31-47ad1c7ada13';
+WHERE id = '3a126f04-1a9e-4127-978b-b6544a099d0d';
 
-/*Pre Alpha C*/
+ /*Level 1 - Section 3*/
 UPDATE content_channelmetadata
 SET "order" = 3
-WHERE id = '20113bf1-ba07-4e08-bcc7-faaca03ade8a';
+WHERE id = '03761fe9-a242-4e9e-bd36-7fbae14dc134';
 
-/*Pre Alpha D*/
+ /*Level 1 - Section 4*/
 UPDATE content_channelmetadata
 SET "order" = 4
-WHERE id = '1700bf9e-7109-4857-abf3-6c04a1963004';
+WHERE id = 'f12e2f67-d589-4b34-bb94-2484d880c4a6';
 
-/*Alpha A*/
+ /*Level 2 - Section 1*/
 UPDATE content_channelmetadata
 SET "order" = 5
-WHERE id = '8784b9f7-8d58-4273-aff5-79b246529215';
+WHERE id = 'f6e8f1f5-69c5-4bc5-8ecb-54d826cd3783';
 
-/*Alpha B*/
+ /*Level 2 - Section 2*/
 UPDATE content_channelmetadata
 SET "order" = 6
-WHERE id = 'cc805378-86cb-498e-b564-242f44c87723';
+WHERE id = '5126a907-e528-4345-847c-8ca749c8cd9b';
 
-/*Alpha C*/
+ /*Level 2 - Section 3*/
 UPDATE content_channelmetadata
 SET "order" = 7
-WHERE id = '7035e792-1ddf-489f-ad45-44c814a199fb';
+WHERE id = 'c32737bf-2647-4dc8-a81d-e3d093075830';
 
-/*Alpha D*/
+ /*Level 2 - Section 4*/
 UPDATE content_channelmetadata
 SET "order" = 8
-WHERE id = '1d8f1428-da33-4779-b956-85c4581186c4';
+WHERE id = '0985900c-047a-4173-8e79-6f19d9b57ed5';
 
-/*Bravo A*/
+ /*Level 3 - Section 1*/
 UPDATE content_channelmetadata
 SET "order" = 9
-WHERE id = '57995474-194c-4068-bfed-1ee16108093f';
+WHERE id = 'c0302f6c-d062-4778-bc8d-d7a2b91d09b4';
 
-/*Bravo B*/
+ /*Level 3 - Section 2*/
 UPDATE content_channelmetadata
 SET "order" = 10
-WHERE id = 'b7214b92-1fd9-4a1c-b758-821919bcd3e0';
+WHERE id = 'ddf25e8f-cde8-4e89-9811-8daffb2d8655';
 
-/*Bravo C*/
+ /*Level 3 - Section 3*/
 UPDATE content_channelmetadata
 SET "order" = 11
-WHERE id = '5aee4435-135b-4039-a3a8-24d96f72bfcb';
+WHERE id = 'e2217b87-2f78-4190-a0a5-5c46e9ee8bc1';
 
-/*Bravo D*/
+ /*Level 3 - Section 4*/
 UPDATE content_channelmetadata
 SET "order" = 12
-WHERE id = '98ab8048-1075-45da-92e3-394409955526';
+WHERE id = 'f908fac9-fe56-44b9-96ab-e25dfbd530bc';
+
+ /*Level 4 - Section 1*/
+UPDATE content_channelmetadata
+SET "order" = 13
+WHERE id = 'd4b44e6d-d4de-450e-bbc7-99085ba656e5';
+
+ /*Level 4 - Section 2*/
+UPDATE content_channelmetadata
+SET "order" = 14
+WHERE id = '763eafc2-4e68-4dbe-8f54-c9a22dd63599';
+
+ /*Level 4 - Section 3*/
+UPDATE content_channelmetadata
+SET "order" = 15
+WHERE id = '7de1d8ec-a487-4856-b508-c1a4f51a839e';
+
+ /*Level 4 - Section 4*/
+UPDATE content_channelmetadata
+SET "order" = 16
+WHERE id = '904d419f-53e2-4b6f-b60d-2aca7e729683';
+
+ /*Level 5 - Section 1*/
+UPDATE content_channelmetadata
+SET "order" = 17
+WHERE id = '5b679315-a02c-4035-9505-fc84701c482d';
+
+ /*Level 5 - Section 2*/
+UPDATE content_channelmetadata
+SET "order" = 18
+WHERE id = 'a5cd9f9f-000e-4145-9a95-875d7dfd3e68';
+
+ /*Level 5 - Section 3*/
+UPDATE content_channelmetadata
+SET "order" = 19
+WHERE id = '6c408923-2a72-447e-abbd-f83b7c5612a4';
+
+ /*Level 5 - Section 4*/
+UPDATE content_channelmetadata
+SET "order" = 20
+WHERE id = '28f4920a-7b44-4e29-af4f-1e8d00ca332b';
+
+ /*Coach Professional Development*/
+UPDATE content_channelmetadata
+SET "order" = 21
+WHERE id = '19ea4c94-ee48-4cb0-b5bb-617f5511f4c1';
+
+ /*Grade 7 Literacy (Zambia)*/
+UPDATE content_channelmetadata
+SET "order" = 22
+WHERE id = '96578ffc-06e4-4a46-b021-540cb217f9c9';
+
