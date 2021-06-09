ec63f0821788d548d79fe01aef735c0c55f905f9
Kapya Sakala <kapsakala@gmail.com>
2021-06-09 14:01:24 +0200

    uncomented lines. call assignmembership procedure with module argument

--- assign_learners.sh
+++ assign_learners.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# update the subscriptions on each of the groups
-# ~/.kolibri_helper_scripts/channel_setup/insert_channel_subscriptions.sh
+# update the subscriptions on each of the groups in case they were modified
+~/.kolibri_helper_scripts/channel_setup/insert_channel_subscriptions.sh
 
 # get the live learners/coaches and insert them into the live learners table
-# python ~/.kolibri_helper_scripts/get_live_learners.py
+python ~/.kolibri_helper_scripts/get_live_learners.py
 
 # call the procedure to assign them into the appropriate groups
-# PGPASSWORD=$BASELINE_DATABASE_PASSWORD psql -h "$BASELINE_DATABASE_HOST" -U "$BASELINE_DATABASE_USER" -d "$BASELINE_DATABASE_NAME" -c "call spassignmembership();"
+PGPASSWORD=$BASELINE_DATABASE_PASSWORD psql -h "$BASELINE_DATABASE_HOST" -U "$BASELINE_DATABASE_USER" -d "$BASELINE_DATABASE_NAME" -c "call spassignmembership('numeracy');"
