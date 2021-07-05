88107c8696c764de5836328e8beafbcaf5fe3a2b
Kapya Sakala <kapsakala@gmail.com>
2021-07-05 14:34:53 +0200

    delete all lessons and quizzes once before starting generation of lessons and quizzes in a loop

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import django
 django.setup()
 
 from kolibri.core.auth.models import Classroom
+from kolibri.core.exams.models import Exam
+from kolibri.core.lessons.models import Lesson
 
 from create_lessons_by_level import create_lessons_by_level
 from create_quizzes_by_level import create_quizzes_by_level
@@ -14,6 +16,16 @@ from create_quizzes_by_level import create_quizzes_by_level
 
 # Get a list of all of the existing classrooms
 classroom_names = [str(c) for c in Classroom.objects.all()]
+
+
+# Delete all lessons
+Lesson.objects.all().delete()
+print("Deleting all existing Lessons")
+
+# Delete all lessons
+Exam.objects.all().delete()
+print("Deleting all existing Quizzes")
+
 
 # Create lessons and Quizzes by level for each
 for classroom in classroom_names:
