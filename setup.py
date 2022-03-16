ecdccca0dd47fe50623c81c2230e8f8f99d0d9a4
Kapya Sakala <kapsakala@gmail.com>
2022-03-16 10:43:17 +0200

    changed imports to match new module and function names. added revision quiz creation to main loop
    (cherry picked from commit 3d32482f9ca3ca9e248abcef47a008a93b8ad242)

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ from kolibri.core.lessons.models import Lesson
 from kolibri.core.exams.models import Exam
 from kolibri.core.auth.models import Classroom, LearnerGroup
 
-from create_lessons_by_level import create_lessons_by_level
-from create_quizzes_by_level import create_quizzes_by_level
+from create_lessons_by_topic import create_lessons_by_topic
+from create_quizzes_by_topic import create_quizzes_by_topic
+from create_level_revision_quizzes import create_revision_quizzes
 
 # To avoid creating duplicate groups, lesson or quizzes
 
@@ -29,5 +30,6 @@ classroom_names = [str(c) for c in Classroom.objects.all()]
 
 # Create lessons and Quizzes by level for each classroom
 for classroom in classroom_names:
-    create_lessons_by_level(classroom)
-    create_quizzes_by_level(classroom)
+    create_lessons_by_topic(classroom)
+    create_quizzes_by_topic(classroom)
+    create_revision_quizzes(classroom)
