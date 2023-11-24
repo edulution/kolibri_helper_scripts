522e3e53f3025e0cb3355c5866052f627b07dbf3
Ntipac <ntch98@gmail.com>
2023-11-24 10:53:47 +0200

    Updated help text

--- channel_setup/insert_channel_subscriptions.sh
+++ channel_setup/insert_channel_subscriptions.sh
@@ -5,7 +5,8 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
   echo "  insert_channel_subscriptions -  Insert numeracy channel subscription"
   echo
   echo "DESCRIPTION"
-  echo "  Inserts subscription for each channel level using the channel subscriptions sql script."
+  echo "  Inserts subscription for each classroom and learner group using the channel "
+  echo "  subscriptions sql script."
   echo 
   echo "Example"
   echo " ./insert_channel_subscriptions.sh"
@@ -13,5 +14,9 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 fi
 
 # Run the channel subscriptions sql on the kolibri database
-PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD psql -h "$KOLIBRI_DATABASE_HOST" -U "$KOLIBRI_DATABASE_USER" -d "$KOLIBRI_DATABASE_NAME" -a -f ~/.kolibri_helper_scripts/sql/channel_subscriptions.sql
+PGPASSWORD=$KOLIBRI_DATABASE_PASSWORD psql \
+-h "$KOLIBRI_DATABASE_HOST" \
+-U "$KOLIBRI_DATABASE_USER" \
+-d "$KOLIBRI_DATABASE_NAME" \
+-a -f ~/.kolibri_helper_scripts/sql/channel_subscriptions.sql
 
