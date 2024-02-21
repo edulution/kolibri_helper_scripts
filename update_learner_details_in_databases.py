6249ebaa2a0d2e7e37850684868a5abb37b59e61
Jeenyhus <mweembadabwitso@gmail.com>
2024-02-21 16:00:35 +0200

    checks if username already exists in facility as usernames are supposed to be unique

--- update_learner_details_in_databases.py
+++ update_learner_details_in_databases.py
@@ -3,14 +3,13 @@ import csv
 import django
 import os
 import sys
+import psycopg2
 import kolibri
 django.setup()
-import psycopg2
 
 from colors import print_colored, colors
 from django.core.exceptions import ObjectDoesNotExist
 from kolibri.core.auth.models import FacilityUser
-
 
 def parse_arguments():
     """
@@ -48,43 +47,49 @@ def update_learner_details(input_file):
                 user_id = user["user_id"]
 
                 try:
-                    # Get user object from the Kolibri database by user_id
+                    # Get user object from the Kolibri database by user_id using Django ORM
                     user_object = FacilityUser.objects.get(id=user_id)
-
-                    # Update user's full name and username in the Kolibri database
-                    if (user_object.full_name == user["full_name"]) and (user_object.username == user["username"]):
-                        unchanged_count += 1
-                        print_colored("User with ID {}: Details have not changed".format(user_id), colors.fg.yellow)
-                    else:
-                        user_object.full_name = user["full_name"]
-                        user_object.username = user["username"]
-                        user_object.save()
-                        # Print success message and update success count if user details were successfully updated
-                        success_count += 1
-                        success_msg = "User with ID {}: Full name has been changed to {}. Username has been changed to {}".format(
-                            user_id, user_object.full_name, user_object.username
-                        )
-                        print_colored(success_msg, colors.fg.green)
-
-                        # Update username in the baseline_testing database using psycopg2 module
-                        cursor = conn.cursor()
-                        # Update username in responses table where user_id matches the user_id from the input file and commit changes
-                        sql_query = "UPDATE responses SET username = %s WHERE user_id = %s"
-                        cursor.execute(sql_query, (user["username"], user_id))
-                        conn.commit()
-                        cursor.close()
-                        success_msg = "User with ID {}: Username updated in baseline_testing database".format(user_id)
-                        print_colored(success_msg, colors.fg.green)
-
-                # Print error message and update not_exist_count if user does not exist
                 except ObjectDoesNotExist:
                     not_exist_count += 1
+                    # Print error message and continue to the next user in the input file
                     error_msg = "User with ID {}: Does not exist".format(user_id)
                     print_colored(error_msg, colors.fg.red)
-
                     continue
 
-    # Print error message if input file does not exist
+                # Check if the username already exists in the Kolibri database and is not the same as the current user's username
+                if FacilityUser.objects.exclude(id=user_id).filter(username=user["username"]).exists():
+                    print_colored(
+                        "Duplicate username. There is already a user called {}".format(user["username"]),
+                        colors.fg.red
+                    )
+                    continue
+                # Check if the full name and username have not changed
+                if (user_object.full_name == user["full_name"]) and (user_object.username == user["username"]):
+                    unchanged_count += 1
+                    print_colored("User with ID {}: Details have not changed".format(user_id), colors.fg.yellow)
+                else:
+                    # Update user details in the Kolibri database using Django ORM
+                    user_object.full_name = user["full_name"]
+                    user_object.username = user["username"]
+                    user_object.save()
+
+                    success_count += 1
+                    success_msg = "User with ID {}: Full name has been changed to {}. Username has been changed to {}".format(
+                        user_id, user_object.full_name, user_object.username
+                    )
+                    print_colored(success_msg, colors.fg.green)
+
+                    # Update username in the baseline_testing database using psycopg2 module
+                    cursor = conn.cursor()
+                    sql_query = "UPDATE responses SET username = %s WHERE user_id = %s"
+                    cursor.execute(sql_query, (user["username"], user_id))
+                    conn.commit()
+                    # Close the cursor after executing the query to avoid memory leaks
+                    cursor.close()
+                    success_msg = "User with ID {}: Username updated in baseline_testing database".format(user_id)
+                    print_colored(success_msg, colors.fg.green)
+
+    # Handle exceptions and close the connection to the database
     except psycopg2.Error as e:
         error_msg = "Error: {}".format(e)
         print_colored(error_msg, colors.fg.red)
