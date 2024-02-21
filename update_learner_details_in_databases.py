047cb415295fb39d73d96d81626fc180a43b48c7
Jeenyhus <mweembadabwitso@gmail.com>
2024-02-21 09:42:28 +0200

    updates learner username in the baseline db

--- update_learner_details_in_databases.py
+++ update_learner_details_in_databases.py
@@ -1,69 +1,124 @@
 import argparse
 import csv
-import os
 import django
+import os
+import sys
 import kolibri
-import psycopg2
 django.setup()
-from kolibri.core.auth.models import FacilityUser
+import psycopg2
+
+from colors import print_colored, colors
 from django.core.exceptions import ObjectDoesNotExist
-from colors import *
+from kolibri.core.auth.models import FacilityUser
+
 
 def parse_arguments():
+    """
+    Parse command-line arguments.
+    """
     parser = argparse.ArgumentParser(description='Update learner details in the database.')
     parser.add_argument('input_file', type=str, help='Path to the input file')
     return parser.parse_args()
 
 def update_learner_details(input_file):
     """
-    Update learner details in both the Kolibri and baseline_testing databases based on the information provided in a CSV file.
+    Update learner details in the database based on the information provided in a CSV file.
 
     Args:
         input_file (str): Path to the input CSV file.
     """
+    success_count = 0
+    unchanged_count = 0
+    not_exist_count = 0
+
     try:
+        # Connect to the baseline_testing database using environmental variables
+        conn = psycopg2.connect(
+            dbname=os.environ.get('BASELINE_DATABASE_NAME'),
+            user=os.environ.get('BASELINE_DATABASE_USER'),
+            password=os.environ.get('BASELINE_DATABASE_PASSWORD'),
+            host=os.environ.get('BASELINE_DATABASE_HOST')
+        )
+        # Read the input file and update learner details
         with open(input_file) as f:
             reader = csv.DictReader(f)
             users = [r for r in reader]
 
             for user in users:
+                user_id = user["user_id"]
+
                 try:
                     # Get user object from the Kolibri database by user_id
-                    user_object = FacilityUser.objects.get(id=user["user_id"])
+                    user_object = FacilityUser.objects.get(id=user_id)
 
                     # Update user's full name and username in the Kolibri database
-                    user_object.full_name = user["full_name"]
-                    user_object.username = user["username"]
-                    user_object.save()
-                    success = "User with ID {}: Full name has been changed to {}. Username has been changed to {}".format(
-                        user_object.id, user_object.full_name, user_object.username
-                    )
-                    print_colored(success, fg.green)
+                    if (user_object.full_name == user["full_name"]) and (user_object.username == user["username"]):
+                        unchanged_count += 1
+                        print_colored("User with ID {}: Details have not changed".format(user_id), colors.fg.yellow)
+                    else:
+                        user_object.full_name = user["full_name"]
+                        user_object.username = user["username"]
+                        user_object.save()
+                        # Print success message and update success count if user details were successfully updated
+                        success_count += 1
+                        success_msg = "User with ID {}: Full name has been changed to {}. Username has been changed to {}".format(
+                            user_id, user_object.full_name, user_object.username
+                        )
+                        print_colored(success_msg, colors.fg.green)
 
-                    # Connect to baseline_testing database and update username in responses table
-                    conn = psycopg2.connect(
-                        dbname=os.environ['BASELINE_DATABASE_NAME'],
-                        user=os.environ['BASELINE_DATABASE_USER'],
-                        password=os.environ['BASELINE_DATABASE_PASSWORD'],
-                        host=os.environ['BASELINE_DATABASE_HOST']
-                    )
-                    cursor = conn.cursor()
-                    sql_query = "UPDATE responses SET username = %s WHERE user_id = %s"
-                    cursor.execute(sql_query, (user["username"], user["user_id"]))
-                    conn.commit()
-                    cursor.close()
-                    conn.close()
+                        # Update username in the baseline_testing database using psycopg2 module
+                        cursor = conn.cursor()
+                        # Update username in responses table where user_id matches the user_id from the input file and commit changes
+                        sql_query = "UPDATE responses SET username = %s WHERE user_id = %s"
+                        cursor.execute(sql_query, (user["username"], user_id))
+                        conn.commit()
+                        cursor.close()
+                        success_msg = "User with ID {}: Username updated in baseline_testing database".format(user_id)
+                        print_colored(success_msg, colors.fg.green)
 
-                    print_colored("Username updated in baseline_testing database", fg.green)
-
+                # Print error message and update not_exist_count if user does not exist
                 except ObjectDoesNotExist:
-                    error = "Error: User with ID {} does not exist".format(user['user_id'])
-                    print_colored(error, fg.red)
+                    not_exist_count += 1
+                    error_msg = "User with ID {}: Does not exist".format(user_id)
+                    print_colored(error_msg, colors.fg.red)
 
                     continue
-    except OSError as e:
-        error = "Error: {}".format(e)
-        print_colored(error, fg.red)
+
+    # Print error message if input file does not exist
+    except psycopg2.Error as e:
+        error_msg = "Error: {}".format(e)
+        print_colored(error_msg, colors.fg.red)
+
+    finally:
+        if conn is not None:
+            conn.close()
+    
+    # Print summary of updates and errors
+    print_summary(success_count, unchanged_count, not_exist_count, len(users))
+
+# Print summary of updates and errors in different colors
+def print_summary(success_count, unchanged_count, not_exist_count, total_users):
+    """
+    Print summary of updates and errors.
+
+    Args:
+        success_count (int): Number of users whose details were successfully updated.
+        unchanged_count (int): Number of users whose details have not changed.
+        not_exist_count (int): Number of users who do not exist.
+        total_users (int): Total number of users processed.
+    """
+    if success_count == 0:
+        print_colored("No learners' details were updated.", colors.fg.red)
+    elif success_count != total_users:
+        print_colored("{} learner(s) had their details successfully updated.".format(success_count), colors.fg.lightgreen)
+    else:
+        print_colored("All {} learner(s) had their details successfully updated.".format(success_count), colors.fg.lightgreen)
+
+    if unchanged_count > 0:
+        print_colored("{} learner(s) details have not changed.".format(unchanged_count), colors.fg.yellow)
+
+    if not_exist_count > 0:
+        print_colored("{} learner(s) do not exist.".format(not_exist_count), colors.fg.red)
 
 if __name__ == '__main__':
     args = parse_arguments()
